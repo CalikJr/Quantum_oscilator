55e38ce8b3d93ee7753abc16b82e08d9f4775769
CalikJr <krzysztof.calik1@gmail.com>
2024-03-25 21:16:58 +0100

    update
    working code update

--- Quantum_oscilator.py
+++ Quantum_oscilator.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 def osc(x):
     return x**2
 
-x_min = float(input())
-x_max = float(input())
-no = int(input())
+x_min = -15#float(input())
+x_max = 15#float(input())
+no = 5500#int(input())
+rm = 4
 
 step = (x_max-x_min)/no
 
@@ -14,15 +15,17 @@ Grid = numpy.linspace(x_min,x_max,no)
 H = numpy.zeros([no,no])
 
 for i in range(no):
-        H[i, i] = (2.0 /(step * step) + (2.0 * osc(Grid[i]) * 1 / (1 * 1))  )
+        H[i, i] = ((2.0 /(step * step) +  osc(Grid[i]))) 
         if i > 0:
             H[i, i - 1] = -1.0/ (step * step)
         if i < (no - 1):
             H[i, i + 1] = -1.0  / (step * step)
 
 
-E, vects = numpy.linalg.eig(H)
-print(E)
-print(vects)
-plt.plot(Grid,vects[:,0])
+E, vects = numpy.linalg.eigh(H)
+#E /= (step*step)
+E /= 2
+print(E[rm])
+#print(vects)
+plt.plot(Grid,vects[:,rm])
 plt.show()
