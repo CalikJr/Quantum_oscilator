1f13d95c3003da20c2b7a53e18b9b9c379732793
CalikJr <krzysztof.calik1@gmail.com>
2024-04-17 18:29:38 +0200

    Adding Time-Dependency
    Added gaussian wave packet described in eigen vectors' basis and added time-dependency part equal to exp(-iEt) for every eigen value

--- Quantum_oscilator.py
+++ Quantum_oscilator.py
@@ -1,11 +1,14 @@
 import numpy 
 import matplotlib.pyplot as plt
+from matplotlib import animation
+import math
+
 def osc(x):
     return x**2
 
 x_min = -15#float(input())
 x_max = 15#float(input())
-no = 5500#int(input())
+no = 1000#int(input())
 rm = 4
 
 step = (x_max-x_min)/no
@@ -27,5 +30,60 @@ E, vects = numpy.linalg.eigh(H)
 E /= 2
 print(E[rm])
 #print(vects)
-plt.plot(Grid,vects[:,rm])
+#plt.plot(Grid,vects[:,rm])
+#plt.show()
+vects=vects/2
+
+a=3
+sigma=3
+P=numpy.array([])
+for x in Grid:
+    P=numpy.append(P, (1/(sigma*math.sqrt(2*math.pi)))*math.exp(-0.5*((x-a)/sigma)**2))
+print(vects.shape)
+print(P.size)
+plt.plot(Grid, P)
+plt.show()
+c_const=numpy.array([])
+for i in range(no):
+    c_const=numpy.append(c_const, numpy.dot(P, vects[:,i]))
+#print(c_const.size)
+#print(c_const)
+
+def packet_state(t):
+    Packet=numpy.array([])
+    for i in range(no):
+        sum=0
+        for j in range(no):
+            sum+=c_const[j]*(math.cos(E[j]*t)+math.sin(E[j]*t))*vects[i, j]
+        Packet=numpy.append(Packet, sum)
+    #print(Packet.size)
+    #print(Packet.shape)
+    return Packet
+#plt.plot(Grid, Packet[:])
+#plt.show()
+
+t_final = 10.0
+t_initial = 0.0
+t = t_initial
+dt = 0.1
+
+fig, ax = plt.subplots()
+ax.set_xlabel('x')
+ax.set_ylabel('P')
+plotLine, = ax.plot(Grid, numpy.zeros(len(Grid))*numpy.NaN, 'r-')
+plotTitle = ax.set_title("t=0")
+ax.set_ylim(-0.3,0.3)
+ax.set_xlim(x_min,x_max)
+
+def animate(t):
+    pp = packet_state(t)
+    plotLine.set_ydata(pp)
+    plotTitle.set_text(f"t = {t:.1f}")
+    #ax.relim() # use if autoscale desired
+    #ax.autoscale()
+    return [plotLine,plotTitle]
+
+
+
+ani = animation.FuncAnimation(fig, func=animate, frames=numpy.arange(t_initial, t_final+dt, dt), blit=True)
 plt.show()
